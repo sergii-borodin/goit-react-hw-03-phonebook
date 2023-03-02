f6749719d57616aa62f462de0f0c9a88fe309f16
BoroDinS <sergey.borodin.lg@gmail.com>
2023-03-02 14:58:56 +0100

    add localStorage saving

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,17 +6,32 @@ import { MainContainer } from "./App.styled";
 
 import { nanoid } from 'nanoid'
 
+import initialContacts from '../../data/contacts';
+
 export class App extends Component {
 
   state = {
-    contacts: [
-    {id: nanoid(), name: 'Rosie Simpson', number: '459-12-56'},
-    {id: nanoid(), name: 'Hermione Kline', number: '443-89-12'},
-    {id: nanoid(), name: 'Eden Clements', number: '645-17-79'},
-    {id: nanoid(), name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: '',
   }
+
+  componentDidMount() { 
+    const savedContacts = localStorage.getItem('contacts');
+
+    if (savedContacts !== null) {
+      const parsedContacts = JSON.parse(savedContacts);
+      this.setState({ contacts: parsedContacts })
+      return
+    }
+    this.setState({contacts: initialContacts });
+    
+  }
+
+  componentDidUpdate(prevProps, prevState) { 
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts) );
+    }
+  } 
   
   addContact = (name, telNumber) => {
     this.setState((prevState) => {
